069c453e9593d482773ea1dd90d65552e52dc3b4
leonardo farinha <leonardofarinhap@gmail.com>
2017-12-04 19:56:31 -0600

    fixed path for phpinfo

--- info.php
+++ info.php
@@ -6,11 +6,11 @@
 
   <nav>
   <ul>
-  <li><a href="credits.php">Credits</a></li>
-  <li><a href="configuration.php">Configuration</a></li>
-  <li><a href="modules.php">Modules</a></li>
-  <li><a href="environment.php">Environment</a></li>
-  <li><a href="variables.php">Variables</a></li>
+  <li><a href="./files/credits.php">Credits</a></li>
+  <li><a href="./files/configuration.php">Configuration</a></li>
+  <li><a href="./files/modules.php">Modules</a></li>
+  <li><a href="./files/environment.php">Environment</a></li>
+  <li><a href="./files/variables.php">Variables</a></li>
   </ul>
   </nav>
   <!-- End Navigation -->
