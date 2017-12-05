b6b03726b78d29354918efac167ac4973fbffc71
leonardo farinha <leonardofarinhap@gmail.com>
2017-12-04 19:46:28 -0600

    again...

--- createfile.php
+++ createfile.php
@@ -5,7 +5,7 @@
  <?php require('./files/nav.php'); ?>
 
  <?php // createfile.php
-  $fh = fopen("./data/testfile.txt", 'x+') or die("Failed to create file");
+  $fh = fopen("/home/fari0022/www/exam2/data/testfile.txt", 'w+') or die("Failed to create file");
 
   $text = <<<_END
 Line 1
@@ -15,6 +15,7 @@ _END;
 
   fwrite($fh, $text) or die("Could not write to file");
   fclose($fh);
+  chmod('./data/testfile.txt', 0755);
   echo "File 'testfile.txt' written successfully";
 ?>
 
