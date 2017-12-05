9dc7383c7ffaf71e5425570302e988e2d6aa38c4
leonardo farinha <leonardofarinhap@gmail.com>
2017-12-04 19:53:28 -0600

    forgot some liks in database.php

--- database.php
+++ database.php
@@ -3,5 +3,12 @@
 <head><title>Leonardo Farinha</title></head>
 <body>
  <?php require('./files/nav.php');?>
+ <nav>
+ <ul>
+ <li><a href="createdb.php">CreateDB</a></li>
+ <li><a href="insert.php">Insert</a></li>
+ <li><a href="display.php">Display</a></li>
+ </ul>
+ </nav>
 </body>
 </html>
