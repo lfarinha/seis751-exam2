2e807a6d95af3741a9ff983bfc4a78f6d73b8e7c
leonardo farinha <leonardofarinhap@gmail.com>
2017-12-04 19:26:00 -0600

    added login

--- auth.php
+++ auth.php
@@ -3,5 +3,24 @@
 <head><title>Leonardo Farinha</title></head>
 <body>
  <?php require('./files/nav.php');?>
+
+ <?php // phpauth.php
+    require('./files/login.php');
+
+   if (isset($_SERVER['PHP_AUTH_USER']) &&
+       isset($_SERVER['PHP_AUTH_PW']))
+   {
+     if ($_SERVER['PHP_AUTH_USER'] == $username &&
+         $_SERVER['PHP_AUTH_PW']   == $password)
+           echo "You are now logged in";
+     else die("Invalid username / password combination");
+   }
+   else
+   {
+     header('WWW-Authenticate: Basic realm="Restricted Section"');
+     header('HTTP/1.0 401 Unauthorized');
+     die ("Please enter your username and password");
+   }
+ ?>
 </body>
 </html>
